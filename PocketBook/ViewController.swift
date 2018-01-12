863e7712a3f81a484286c822eb562eedc16a0b4c
Chi Mai <chi.mai@naamtech.com>
2018-01-12 16:08:09 +1100

    Initial Commit

---
+++ PocketBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PocketBook
+//
+//  Created by Chi on 12/1/18.
+//  Copyright © 2018 dmc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
